b7e779a4de7759ea0ed3cba6d005c9736244b3aa
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-03-09 11:00:53 +0100

    Error page if path doesnt exist

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,19 @@ import React from "react";
 import Home from "./pages/Home";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Error from "./pages/Error";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 };
 
